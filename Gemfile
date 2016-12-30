60aa8243196b313e7370a4737de9dace0e73833f
Gearnode <bfrimin@student.42.fr>
2016-12-30 16:52:15 +0100

    code coverage

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in fail_to_ban.gemspec
 gemspec
+
+group :test do
+  gem 'simplecov'
+  gem 'codeclimate-test-reporter', '~> 1.0.0'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "fail_to_ban"
+require 'rspec'
+
+if ENV.fetch('CODECLIMATE_REPO_TOKEN', false)
+  require 'simplecov'
+  SimpleCov.start
+end
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'fail_to_ban'
