579f009ba9c1378effc11607426682fa650ca025
eraleis <vatourni@gmail.com>
2018-06-21 17:08:49 +0200

    Add some modularity to configuration

--- lib/fail_to_ban.rb
+++ lib/fail_to_ban.rb
@@ -1,30 +1,41 @@
-require "fail_to_ban/version"
+require 'fail_to_ban/version'
 
 class FailToBan
-
   PROTECT_DURATION = 60 * 60 * 24 # eq 24 hours in seconds
 
-  PERMIT_FAIL_ATTEMPS = 3
-
   JITTER_VARIANCE = 0.2
-  BACKOFF_RATE = 15 # seconds
 
   attr_reader(:unique_key)
 
-  def initialize(storage:, unique_key:)
+  def initialize(storage:, unique_key:, config: {})
     @storage = storage
     @unique_key = unique_key
+    @config = default_config.merge(config)
     @id = "fail_to_ban:#{@unique_key}"
   end
 
+  def default_config
+    {
+      permitted_attempts: 3,
+      backoff_step: 15, # in seconds
+      backoff_cap: nil
+    }
+  end
+
   def protect
+    warn '[DEPRECATION] `protect` is deprecated.  Please use `attempt` instead.'
+    attempt
+  end
+
+  def attempt
     return :blocked if blocked?
     increment_fail_attempts
+    puts backoff
     :ok
   end
 
   def blocked?
-    return false if retry_count < PERMIT_FAIL_ATTEMPS || Time.now.to_i >= unlock_at
+    return false if retry_count < @config[:permitted_attempts] || Time.now.to_i >= unlock_at
     true
   end
 
@@ -35,6 +46,10 @@ class FailToBan
 
   def unlock_at
     @storage.hget(id, 'unlock_at').to_i
+  end
+
+  def unlock_in
+    unlock_at - Time.now.to_i
   end
 
   private
@@ -51,11 +66,18 @@ class FailToBan
     @storage.hget(id, 'retry_count').to_i
   end
 
-  # Backoff : after 3 failed attempts there is a 15 seconds wait
-  # If it fails again then it's 30 seconds, then 45,
-  # In any case, set a +/- 10% jitter on the wait (e.g 14, 28, 47, ...)
+  # Backoff : after the permitted_attempts is exceeded is a x seconds wait
+  # If it fails again then it's x * 2 seconds, then x * 3,
+  # If the back_time exceeds the backoff cap, then it stop growing
+  # In any case, set a +/- 10% jitter on the wait (e.g with a backoff step of 15: 14, 28, 47, ...)
   def backoff
-    BACKOFF_RATE * (retry_count - PERMIT_FAIL_ATTEMPS) * (1 + JITTER_VARIANCE * (rand - 0.5))
+    variance = (1 + JITTER_VARIANCE * (rand - 0.5))
+    back_time = @config[:backoff_step] * (retry_count - @config[:permitted_attempts] + 1) * variance
+    if !@config[:backoff_cap].nil? && back_time > @config[:backoff_cap] * variance
+      @config[:backoff_cap] * variance
+    else
+      back_time
+    end
   end
 
 end
--- spec/fail_to_ban_spec.rb
+++ spec/fail_to_ban_spec.rb
@@ -1,7 +1,6 @@
-require "spec_helper"
+require 'spec_helper'
 
 class NullStorage
-
   def initialize(null_data = {})
     @null_data = null_data
   end
@@ -31,79 +30,100 @@ class NullStorage
   def del(k)
     @null_data.delete(k)
   end
-
 end
 
 describe FailToBan do
-
   it "has a version number" do
     expect(FailToBan::VERSION).not_to be nil
   end
 
   let(:storage) { NullStorage.new(data) }
 
-  describe '#protect' do
-
-    subject { described_class.new(storage: storage, unique_key: key).protect }
+  describe '#attempt' do
+    subject do
+      described_class.new(
+        storage: storage,
+        unique_key: key,
+        config: {
+          permitted_attempts: 3,
+          backoff_step: 60 * 60 * 24,
+          backoff_cap: 60 * 60 * 48
+        }
+      )
+    end
 
     let(:key) { 'dev@jobteaser.com' }
 
-    context 'when retry < 3' do
-
+    context 'when retry does not exceeds the permitted_attempts' do
       let(:data) do
         {}
       end
 
       it 'increments retry_count' do
-        expect { subject }.to change { storage.hget("fail_to_ban:#{key}", "retry_count") }.from(nil).to(1)
+        expect { subject.attempt }.to change { storage.hget("fail_to_ban:#{key}", "retry_count") }.from(nil).to(1)
       end
 
       it 'returns :ok' do
-        expect(subject).to be :ok
+        expect(subject.attempt).to be :ok
       end
-
     end
 
-    context 'when retry > 3' do
+    context 'when retry exceeds the permitted_attempts' do
       let(:data) do
         {}
       end
 
       it 'returns :blocked' do
         allow(Time).to receive(:now).and_return(Time.new('2015', '01', '01'))
-        foo = described_class.new(storage: storage, unique_key: key)
+        foo = described_class.new(
+          storage: storage,
+          unique_key: key
+        )
 
-        (described_class::PERMIT_FAIL_ATTEMPS + 1).times do
-          expect(foo.protect).to eq :ok
-        end
-        expect(foo.protect).to eq :blocked
+        3.times { expect(foo.attempt).to eq :ok }
+        expect(foo.attempt).to eq :blocked
       end
-
     end
 
-    context 'when unlock_at have expire' do
-
+    context 'when unlock_at have expired' do
       let(:data) do
         { "fail_to_ban:#{key}" => { "retry_count" => "5", "unlock_at" => Time.new('2014', '12', '12').to_i.to_s } }
       end
 
       it 'returns :ok' do
         allow(Time).to receive(:now).and_return(Time.new('2015', '01', '01'))
-        expect(subject).to eq :ok
+        expect(subject.attempt).to eq :ok
       end
-
     end
 
+    context 'when it reaches the backoff cap' do
+      let(:data) do
+        { "fail_to_ban:#{key}" => { "retry_count" => "6", "unlock_at" => Time.new('2014', '12', '12').to_i.to_s } }
+      end
+
+      it 'does not increment the backoff time' do
+        stub_const('::FailToBan::JITTER_VARIANCE', 0)
+        allow(Time).to receive(:now).and_return(Time.new('2014', '12', '12'))
+        expect(subject.attempt).to eq :ok
+        expect(subject.attempt).to eq :blocked
+
+        expected_unlock_in = subject.unlock_in
+
+        allow(Time).to receive(:now).and_return(Time.new('2015', '01', '01'))
+        expect(subject.attempt).to eq :ok
+        expect(subject.attempt).to eq :blocked
+
+        expect(subject.unlock_in).to eq(expected_unlock_in)
+      end
+    end
   end
 
   describe '#blocked?' do
-
     subject { described_class.new(storage: storage, unique_key: key).blocked? }
 
     let(:key) { 'dev@jobteaser.com' }
 
     context 'when key is not blocked' do
-
       let(:data) do
         {}
       end
@@ -111,11 +131,9 @@ describe FailToBan do
       it 'returns false' do
         expect(subject).to be false
       end
-
     end
 
     context 'when key is blocked' do
-
       let(:data) do
         { "fail_to_ban:#{key}" => {"retry_count" => "4", "unlock_at" => " 1477401039" }  }
       end
@@ -124,13 +142,10 @@ describe FailToBan do
         allow(Time).to receive(:now).and_return(Time.new('2015', '01', '01'))
         expect(subject).to be true
       end
-
     end
-
   end
 
   describe '#reset!' do
-
     subject { described_class.new(storage: storage, unique_key: key).reset }
 
     let(:key) { 'dev@jobteaser.com' }
@@ -146,7 +161,5 @@ describe FailToBan do
     it 'removes unlock_at from storage' do
       expect { subject }.to change { storage.hget("fail_to_ban:#{key}", "unlock_at") }.from('0').to(nil)
     end
-
   end
-
 end
